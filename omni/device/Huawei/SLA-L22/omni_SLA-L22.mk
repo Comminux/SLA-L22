ca56fdca630931c41e4d6c413e92570ec8d47841
Lourens-Rich <lourens@openmailbox.org>
2018-01-17 16:32:38 +0300

    Created device folder with simple configuration

---
+++ omni/device/Huawei/SLA-L22/omni_SLA-L22.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/Huawei/SLA-L22/SLA-L22.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := generic_a53_32
+PRODUCT_NAME := generic_a53_32
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := generic_a53_32
+#PRODUCT_MANUFACTURER := 
